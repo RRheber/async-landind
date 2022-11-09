060167247791211a1c35bf1ebde84ef2fd8f2ace
hraudales <heber.raudac27@outlook.com>
2022-11-08 19:40:20 -0600

    agregando archivos src

---
+++ src/assets/main.js
@@ -0,0 +1,45 @@
+const API =
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCUB0_U7Z3o9U2dGG6dYVA3g&part=snippet%2Cid&order=date&maxResults=5";
+const content = null || document.getElementById("content");
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f7487c9d8emshfdb29cb919a689cp17b27fjsn51b3aa809250",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items
+      .map(
+        (video) => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `
+      )
+      .slice(0, 4)
+      .join("")}
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
